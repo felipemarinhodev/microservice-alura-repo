6a8982f2c2b1df5513f7e9d623388dc80faee690
Felipe Marinho <felipmarinho@gmail.com>
2020-09-03 21:31:32 -0300

    Adjustments for work with fornecedor project

--- loja/src/main/java/dev/felipemarinho/microservice/loja/controller/CompraController.java
+++ loja/src/main/java/dev/felipemarinho/microservice/loja/controller/CompraController.java
@@ -1,18 +1,23 @@
 package dev.felipemarinho.microservice.loja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.felipemarinho.microservice.loja.controller.dto.CompraDTO;
+import dev.felipemarinho.microservice.loja.service.CompraService;
 
 @RestController
 @RequestMapping("/compra")
 public class CompraController {
+	
+	@Autowired
+	private CompraService compraService;
 
 	@PostMapping
 	public void realizarCompra(@RequestBody CompraDTO compra) {
-		
+		compraService.realizarCompra(compra);
 	}
 }
