948392d1646c94f97c428d223b467cd0a958cda2
Felipe Marinho <felipmarinho@gmail.com>
2020-09-07 14:28:14 -0300

    Improving for using feign

---
+++ loja/src/main/java/dev/felipemarinho/microservice/loja/client/FornecedorClient.java
@@ -0,0 +1,16 @@
+package dev.felipemarinho.microservice.loja.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dev.felipemarinho.microservice.loja.dto.InfoFornecedorDTO;
+
+@FeignClient("fornecedor")
+public interface FornecedorClient {
+	
+	@RequestMapping("/info/{estado}")
+	InfoFornecedorDTO getInfoPorEstado(@PathVariable String estado);
+
+}
+;
---
+++ loja/src/main/java/dev/felipemarinho/microservice/loja/dto/InfoFornecedorDTO.java
@@ -0,0 +1,13 @@
+package dev.felipemarinho.microservice.loja.dto;
+
+public class InfoFornecedorDTO {
+	
+	private String endereco;
+
+	public String getEndereco() {
+		return endereco;
+	}
+	
+	
+
+}
