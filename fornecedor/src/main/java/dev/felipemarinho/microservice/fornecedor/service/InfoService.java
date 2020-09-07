45c07f53289f301f2303163242761069868f14d6
Felipe Marinho <felipmarinho@gmail.com>
2020-09-07 18:00:44 -0300

    Adding logs in the applications

--- fornecedor/src/main/java/dev/felipemarinho/microservice/fornecedor/service/InfoService.java
+++ fornecedor/src/main/java/dev/felipemarinho/microservice/fornecedor/service/InfoService.java
@@ -1,18 +1,24 @@
 package dev.felipemarinho.microservice.fornecedor.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import dev.felipemarinho.microservice.fornecedor.controller.InfoController;
 import dev.felipemarinho.microservice.fornecedor.model.InfoFornecedor;
 import dev.felipemarinho.microservice.fornecedor.repository.InfoRepository;
 
 @Service
 public class InfoService {
+	
+	Logger LOG = LoggerFactory.getLogger(InfoService.class);
 
 	@Autowired
 	private InfoRepository infoRepository;
 	
 	public InfoFornecedor getInfoPorEstado(String estado) {
+		LOG.info("Buscando informações do fornecedor de {}", estado);
 		 return infoRepository.findByEstado(estado);
 	}
 
--- loja/src/main/java/dev/felipemarinho/microservice/loja/service/CompraService.java
+++ loja/src/main/java/dev/felipemarinho/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package dev.felipemarinho.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,22 +14,25 @@ import dev.felipemarinho.microservice.loja.dto.InfoPedidoDTO;
 @Service
 public class CompraService {
 	
+	Logger LOG = LoggerFactory.getLogger(Compra.class);
+	
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
 	public Compra realizarCompra(CompraDTO compra) {
+		final String estado = compra.getEndereco().getEstado();
+		LOG.info("Buscando informações do fornecedor de {}", estado);
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
 		
-		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
-		
+		LOG.info("Realizando um pedido");
 		InfoPedidoDTO pedido = fornecedorClient.realizarPedido(compra.getItens());
 		
-		System.out.println(info.getEndereco());
-		
 		Compra compraSalva = new Compra();
-		
 		compraSalva.setPedidoId(pedido.getId());
 		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
-		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		compraSalva.setEnderecoDestino(info.getEndereco());
+		LOG.info("Pedido {} realizado com sucesso!", compraSalva.getPedidoId());
+		
 		return compraSalva;
 	}
 
